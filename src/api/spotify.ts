698066756c9546233a90035a3b1a9b1b8a9ced7b
Valik <valik3201@gmail.com>
2024-03-10 08:37:11 +0100

    feat: add base URL configuration

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -1,15 +1,14 @@
 import axios from "axios";
 
+axios.defaults.baseURL = "https://api.spotify.com/v1";
+
 export const fetchNewReleases = async (token: string) => {
   try {
-    const response = await axios.get(
-      "https://api.spotify.com/v1/browse/new-releases",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const response = await axios.get("/browse/new-releases", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
     return response.data;
   } catch (error) {
