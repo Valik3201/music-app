737bb3df04accfc544c365425160d68067782674
Valik <valik3201@gmail.com>
2024-03-11 22:34:44 +0100

    test: validate authentication in production environment

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -16,3 +16,46 @@ export const fetchNewReleases = async (token: string) => {
     return null;
   }
 };
+
+export const searchItems = async (
+  token: string,
+  query: string,
+  types: string[],
+  market: string = "US",
+  limit: number = 20
+) => {
+  try {
+    const response = await axios.get("/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: query,
+        type: types.join(","),
+        market: market,
+        limit: limit,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error searching items:", error);
+    return null;
+  }
+};
+
+export const getUserSavedAlbums = async (accessToken: string) => {
+  try {
+    const response = await axios.get("https://api.spotify.com/v1/me/albums", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    console.log("User saved albums:", response.data);
+    return response.data;
+    // Здесь вы можете обработать данные о сохраненных альбомах пользователя
+  } catch (error) {
+    console.error("Error fetching user saved albums:", error);
+  }
+};
