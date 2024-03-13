c682ac28ec6121b616f956644b773f0f19a8a99a
Valik <valik3201@gmail.com>
2024-03-13 19:37:36 +0100

    refactor: update scope for requests

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,4 +3,5 @@ export const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 export const redirectUri = "https://music-app-ts.netlify.app";
 export const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 export const tokenEndpoint = "https://accounts.spotify.com/api/token";
-export const scope = "user-read-private user-read-email user-library-read";
+export const scope =
+  "user-read-private user-read-email user-library-read playlist-modify-public playlist-modify-private";
