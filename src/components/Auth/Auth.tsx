1b10333981373fb3333828f001ff19a0fb83f099
Valik <valik3201@gmail.com>
2024-03-16 23:27:10 +0100

    feat(authentication): add modal window for auto logout on session expiration

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -2,13 +2,17 @@ import { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import { exchangeToken, fetchUserData } from "../../redux/auth/authOperations";
 import HeroSection from "../HeroSection/HeroSection";
-import Root from "../../Root/Root";
-import AutoLogout from "./AutoLgout";
+import Root from "../Root/Root";
+import AutoLogout from "../AutoLogout/AutoLogout";
+import ModalSessionExpired from "../ModalSessionExpired/ModalSessionExpired";
 
 const Auth: React.FC = () => {
   const currentToken = useAppSelector((state) => state.auth.currentToken);
   const user = useAppSelector((state) => state.auth.user);
   const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
+  const isSessionExpired = useAppSelector(
+    (state) => state.auth.isSessionExpired
+  );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -45,6 +49,8 @@ const Auth: React.FC = () => {
       {currentToken && currentToken.expires_in && (
         <AutoLogout tokenExpirationDate={new Date(currentToken.expires_in)} />
       )}
+
+      {isSessionExpired && <ModalSessionExpired />}
     </div>
   );
 };
