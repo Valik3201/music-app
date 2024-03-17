5ae75ad9a323b176aebf3a60a6eb2bfcbca930c6
Valik <valik3201@gmail.com>
2024-03-17 11:25:28 +0100

    fix(icons): сorrect attributes in icon component

--- src/icons/flowbite.tsx
+++ src/icons/flowbite.tsx
@@ -199,9 +199,9 @@ export const PlayIconSolid = () => (
     viewBox="0 0 24 24"
   >
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z"
-      clip-rule="evenodd"
+      clipRule="evenodd"
     />
   </svg>
 );
