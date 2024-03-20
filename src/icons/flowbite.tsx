ce9e191fca42b643e53187495ee7434ac0481ba5
Valik <valik3201@gmail.com>
2024-03-20 11:32:17 +0100

    feat(icons): add bar icon

--- src/icons/flowbite.tsx
+++ src/icons/flowbite.tsx
@@ -228,7 +228,7 @@ export const PlusIcon = () => (
 
 export const CheckIcon = () => (
   <svg
-    className="w-5 h-5 absolute top-0.5 left-0 z-10 invisible peer-checked:visible text-black bg-green border-green rounded"
+    className="w-5 h-5 p-0.5 absolute top-0.5 left-0 z-10 invisible peer-checked:visible text-black bg-green border-green rounded"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -244,4 +244,35 @@ export const CheckIcon = () => (
       d="M5 11.917 9.724 16.5 19 7.5"
     />
   </svg>
+);
+
+export const BarIcon = () => (
+  <>
+    <svg
+      aria-hidden="true"
+      className="w-6 h-6"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+    <svg
+      aria-hidden="true"
+      className="hidden w-6 h-6"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  </>
 );
