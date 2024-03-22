36176abeec56fc829abfe3621f0dea42ba312912
Valik <valik3201@gmail.com>
2024-03-22 19:49:20 +0100

    chore(ui): create `ui` folder for styling components

--- src/components/ui/icons/flowbite.tsx
+++ src/components/ui/icons/flowbite.tsx
@@ -20,7 +20,7 @@ export const HomeIconOutline = () => (
 
 export const HomeIconSolid = () => (
   <svg
-    className="w-6 h-6"
+    className="w-6 h-6 text-green"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -57,7 +57,7 @@ export const SearchIconOutline = () => (
 
 export const SearchIconSolid = () => (
   <svg
-    className="w-6 h-6"
+    className="w-6 h-6 text-green"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -96,7 +96,7 @@ export const GridIconOutline = () => (
 
 export const GridIconSolid = () => (
   <svg
-    className="w-6 h-6"
+    className="w-6 h-6 text-green"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -134,7 +134,7 @@ export const GridPlusIconOutline = () => (
 
 export const GridPlusIconSolid = () => (
   <svg
-    className="w-6 h-6"
+    className="w-6 h-6 text-green"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -172,7 +172,7 @@ export const ListMusicOutline = () => (
 
 export const ListMusicSolid = () => (
   <svg
-    className="w-6 h-6"
+    className="w-6 h-6 text-green"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -190,7 +190,7 @@ export const ListMusicSolid = () => (
 
 export const PlayIconSolid = () => (
   <svg
-    className="w-8 h-8 fill-green"
+    className="w-8 h-8 text-green"
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -206,9 +206,9 @@ export const PlayIconSolid = () => (
   </svg>
 );
 
-export const PlusIcon = () => (
+export const PlusIcon = ({ green }: { green?: boolean }) => (
   <svg
-    className="w-6 h-6 text-gray-800 dark:text-white"
+    className={`w-6 h-6 ${green ? "text-green" : ""}`}
     aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -256,9 +256,9 @@ export const BarIcon = () => (
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       ></path>
     </svg>
     <svg
@@ -269,10 +269,52 @@ export const BarIcon = () => (
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       ></path>
     </svg>
   </>
+);
+
+export const StarIconOutline = () => (
+  <svg
+    className="w-6 h-6"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeWidth="2"
+      d="M11.083 5.104c.35-.8 1.485-.8 1.834 0l1.752 4.022a1 1 0 0 0 .84.597l4.463.342c.9.069 1.255 1.2.556 1.771l-3.33 2.723a1 1 0 0 0-.337 1.016l1.03 4.119c.214.858-.71 1.552-1.474 1.106l-3.913-2.281a1 1 0 0 0-1.008 0L7.583 20.8c-.764.446-1.688-.248-1.474-1.106l1.03-4.119A1 1 0 0 0 6.8 14.56l-3.33-2.723c-.698-.571-.342-1.702.557-1.771l4.462-.342a1 1 0 0 0 .84-.597l1.753-4.022Z"
+    />
+  </svg>
+);
+
+interface StarIconSolidProps {
+  size?: string;
+}
+
+export const StarIconSolid: React.FC<StarIconSolidProps> = ({
+  size = "w-6 h-6",
+}) => (
+  <svg
+    className={`${size} text-green`}
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+  </svg>
+);
+
+export const MusicIcon = () => (
+  <svg role="img" viewBox="0 0 24 24" className="w-20 h-20 fill-silver-900">
+    <path d="M6 3h15v15.167a3.5 3.5 0 1 1-3.5-3.5H19V5H8v13.167a3.5 3.5 0 1 1-3.5-3.5H6V3zm0 13.667H4.5a1.5 1.5 0 1 0 1.5 1.5v-1.5zm13 0h-1.5a1.5 1.5 0 1 0 1.5 1.5v-1.5z"></path>
+  </svg>
 );
