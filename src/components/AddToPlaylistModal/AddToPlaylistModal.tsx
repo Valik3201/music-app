801d8291012a77cebef0b2e040f996b374a0a4d5
Valik <valik3201@gmail.com>
2024-03-23 18:29:17 +0100

    feat(playlists): implement ability to add songs to user playlists

--- src/components/AddToPlaylistModal/AddToPlaylistModal.tsx
+++ src/components/AddToPlaylistModal/AddToPlaylistModal.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAppSelector } from "../../redux/hooks";
 
 import { addItemsToPlaylist } from "../../api/spotify";
+import { ListMusicOutline, PlusIcon } from "../ui/icons/flowbite";
 
 const AddToPlaylistModal: React.FC<{ uri: string }> = ({ uri }) => {
   const currentToken = useAppSelector((state) => state.auth.currentToken);
@@ -25,55 +26,51 @@ const AddToPlaylistModal: React.FC<{ uri: string }> = ({ uri }) => {
 
   return (
     <>
-      <button onClick={toggleModal}>Add</button>
+      <button
+        onClick={toggleModal}
+        className="p-2 rounded-lg hover:bg-shark transition duration-200 ease-in-out"
+      >
+        <PlusIcon />
+      </button>
 
       {isOpen && (
         <div
-          id="default-modal"
-          tabIndex={-1}
           aria-hidden="true"
           className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-50"
         >
-          <div className="relative p-4 w-full max-w-2xl max-h-full">
+          <div className="relative p-4 w-full max-w-md max-h-full">
             <div className="relative bg-shark rounded-lg shadow">
               <div className="flex items-start justify-between p-4 md:p-5 border-b rounded-t">
-                <h2 className="text-white text-3xl font-black pb-4">
+                <h2 className="text-white text-3xl font-black">
                   Add to playlist
                 </h2>
 
                 <button
                   onClick={toggleModal}
                   type="button"
-                  className="text-white p-2 rounded-lg hover:bg-black/50"
+                  className="text-white p-2 rounded-lg hover:bg-black/50 transition duration-200 ease-in-out"
                   data-modal-hide="default-modal"
                 >
-                  <svg
-                    className="w-3 h-3"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 14"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                    />
-                  </svg>
+                  <div className="rotate-45">
+                    <PlusIcon />
+                  </div>
+
                   <span className="sr-only">Close modal</span>
                 </button>
               </div>
 
-              <div className="p-4 md:p-5 space-y-4">
+              <div className="p-4 md:p-5 space-y-4 overflow-y-auto max-h-96">
                 <ul>
                   {userPlaylists &&
                     user &&
                     userPlaylists.map((item) =>
                       item.owner.display_name === user.display_name ? (
                         <li key={item.id}>
-                          <button onClick={() => handlePlaylistSelect(item.id)}>
+                          <button
+                            onClick={() => handlePlaylistSelect(item.id)}
+                            className="flex items-center gap-2 p-2 w-full text-base rounded-lg hover:bg-black/40 transition duration-200 ease-in-out"
+                          >
+                            <ListMusicOutline />
                             {item.name}
                           </button>
                         </li>
