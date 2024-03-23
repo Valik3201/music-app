1b2b7abd46849d4fe726d9d2ee28e8a6cabb7bad
Valik <valik3201@gmail.com>
2024-03-23 18:29:05 +0100

    refactor(components): update useEffect hooks for testing purposes

--- src/components/Playlists/Playlists.tsx
+++ src/components/Playlists/Playlists.tsx
@@ -1,26 +1,11 @@
-import { useEffect } from "react";
-import { useAppSelector, useAppDispatch } from "../../redux/hooks";
-import { getUserPlaylists } from "../../redux/userData/userDataOperations";
+import { useAppSelector } from "../../redux/hooks";
+
 import { Link } from "react-router-dom";
 import PlaylistCover from "../ui/PlaylistCover/PlaylistCover";
 
 const Playlists: React.FC = () => {
-  const currentToken = useAppSelector((state) => state.auth.currentToken);
   const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
-  const user = useAppSelector((state) => state.auth.user);
-  const userPlailists = useAppSelector((state) => state.data.playlists);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (currentToken && currentToken.access_token && user && user.id) {
-      dispatch(
-        getUserPlaylists({
-          accessToken: currentToken.access_token,
-          userID: user.id,
-        })
-      );
-    }
-  }, [currentToken, user]);
+  const userPlaylists = useAppSelector((state) => state.data.playlists);
 
   return (
     <>
@@ -29,22 +14,22 @@ const Playlists: React.FC = () => {
           <h2 className="text-3xl font-black pb-4">Playlists</h2>
 
           <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-2">
-            {userPlailists &&
-              userPlailists.map((playlist: any) => (
+            {userPlaylists &&
+              userPlaylists.map((playlist: any) => (
                 <li
                   key={playlist.id}
                   className="hover:opacity-70 transition duration-200 ease-in-out"
                 >
                   <Link to={`/playlist/${playlist.id}`}>
                     <div className="bg-shark aspect-square rounded-lg mb-2 hover:opacity-70 transition duration-200 ease-in-out">
-                      {playlist.images[0]?.url ? (
+                      {playlist.images && playlist.images[0].url ? (
                         <img
                           src={playlist.images[0].url}
                           alt={playlist.name}
                           className="object-cover aspect-square rounded-lg"
                         />
                       ) : (
-                        <PlaylistCover />
+                        <PlaylistCover size={"w-full h-full"} />
                       )}
                     </div>
 
