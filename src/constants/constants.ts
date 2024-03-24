1f7611da08115a1a17be7b16b23693b6b8f31949
Valik <valik3201@gmail.com>
2024-03-24 13:25:20 +0100

    feat(auth): update redirect URI

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 export const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-export const redirectUri = "http://localhost:5173";
-// export const redirectUri = "https://music-app-ts.netlify.app";
+// export const redirectUri = "http://localhost:5173";
+export const redirectUri = "https://music-app-ts.netlify.app";
 export const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 export const tokenEndpoint = "https://accounts.spotify.com/api/token";
 export const scope =
